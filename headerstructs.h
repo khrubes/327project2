a6cab17729e45447c97287e5f1aac19f6213c943
Kelsey Hrubes <kelseyhrubes@gmail.com>
2015-09-24 09:28:31 -0500

    start adding cases for parse_header

--- headerstructs.h
+++ headerstructs.h
@@ -1,107 +1,113 @@
 #ifndef HEADER_STRUCTS
 #define HEADER_STRUCTS
-#include <stdbool.h>
+#include <string.h>
 
-struct IDentification_Name_Header {
-	 bool (*isValid)(char*);
-	 char* typePtr;
-	 char* structNameString;
-};
+void parse_header(char* header_struct_name) {
+	if (strcmp(*header_struct_name, "IDentification_Name") == 0) {
 
-struct USAF_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+	}
+}
 
-struct NCDC_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct IDentification_Name_Header {
+// 	 bool (*isValid)(char*);
+// 	 char* typePtr;
+// 	 char* structNameString;
+// };
 
-struct Date_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct USAF_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct HrMn_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct NCDC_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct I_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Date_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Type_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct HrMn_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct QCP_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct I_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Wind_Dir_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Type_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Wind_Spd_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct QCP_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Wind_Spd_Q_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Wind_Dir_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Temp_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Wind_Spd_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Temp_Q_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Wind_Spd_Q_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Dewpt_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Temp_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Dewpt_Q_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Temp_Q_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct Slp_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Dewpt_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
-struct RHx_Header {
-	bool (*isValid)(char*);
-	char* typePtr;
-	char* structNameString;
-};
+// struct Dewpt_Q_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
+
+// struct Slp_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
+
+// struct RHx_Header {
+// 	bool (*isValid)(char*);
+// 	char* typePtr;
+// 	char* structNameString;
+// };
 
 #endif
--- weatherio.c
+++ weatherio.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
 #include "headerstructs.h"
-int validate_format( char * fileName){
-    	// //call validate_format( char * ) with argv[1]
-     //    FILE *file = fopen(fileName , "r" );
-        
-     //    /* fopen returns 0, the NULL pointer, on failure */
-     //    if ( file == NULL )
-     //    {
-     //        printf( "Could not open file\n" );
-     //    }
-     //    else
-     //    {
-     //        int x;
-     //        while  ( ( x = fgetc( file ) ) != EOF )
-     //        {
-     //            printf( "%c", x );
-     //        }
-     //        printf("\n");
-     //        fclose( file );
-     //    }
 
-    FILE *fp;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    fp = fopen(fileName, "r");
-    if (fp == NULL)
-        return 1;
-
-    while ((read = getline(&line, &len, fp)) != -1) {
-        printf("Retrieved line of length %zu :\n", read);
-        printf("%s \n", line);
+int header_columns( char * ) {
+    int numCommas = 0;
+    FILE *file = fopen(fileName , "r" );
+    
+    if ( file != NULL )
+    {
+        int c;
+        int numCommas = 0;
+        while  ( ( c = fgetc( file ) ) != EOF )
+        {
+            if(c == ',') {
+                numCommas++;
+            }
+        }
+        fclose( file );
     }
+    return numCommas;
+}
 
-    free(line);
+int validate_format( char * fileName){
+    int numColumns = header_columns(fileName);
+
+    // FILE *fp;
+    // char *line = NULL;
+    // size_t len = 0;
+    // ssize_t read;
+
+    // fp = fopen(fileName, "r");
+    // if (fp == NULL)
+    //     return 1;
+
+    // while ((read = getline(&line, &len, fp)) != -1) {
+    //     printf("Retrieved line of length %zu :\n", read);
+    //     printf("%s \n", line);
+    // }
+
+    // free(line);
 }
